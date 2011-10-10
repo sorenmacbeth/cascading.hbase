4648da3ed20c6a7ea73fad7cff1c8c1aa0ba9043
Matan <matan@taykey.com>
2011-10-10 14:29:49 +0200

    Added: Create CFs when table exists but some of the required CFs don't exist.

--- src/main/java/cascading/hbase/HBaseTap.java
+++ src/main/java/cascading/hbase/HBaseTap.java
@@ -15,6 +15,8 @@ package cascading.hbase;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 
 import cascading.tap.SinkMode;
 import cascading.tap.Tap;
@@ -23,6 +25,8 @@ import cascading.tap.hadoop.TapCollector;
 import cascading.tap.hadoop.TapIterator;
 import cascading.tuple.TupleEntryCollector;
 import cascading.tuple.TupleEntryIterator;
+
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.HColumnDescriptor;
@@ -30,11 +34,14 @@ import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.MasterNotRunningException;
 import org.apache.hadoop.hbase.ZooKeeperConnectionException;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.mapred.TableOutputFormat;
 import org.apache.hadoop.mapred.FileInputFormat;
 import org.apache.hadoop.mapred.JobConf;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+
 
 /**
  * The HBaseTap class is a {@link Tap} subclass. It is used in conjunction with the {@HBaseFullScheme}
@@ -101,24 +108,80 @@ public class HBaseTap extends Tap {
 
 		return hBaseAdmin;
 	}
+	
+	public static HTable openTable(String tableName, String... coulmnFamilyArray) throws IOException 
+	{
+		Configuration config = HBaseConfiguration.create();
+
+		HBaseAdmin hbase = new HBaseAdmin(config);
+		byte[] tableNameByte = tableName.getBytes();
+
+		if (hbase.tableExists(tableNameByte))
+		{
+			// table exists
+			LOG.debug("Table: " + tableName + " already exists!");
+			if (hbase.isTableEnabled(tableNameByte))
+			{
+				// table enabled
+
+				HTable hTable =  new HTable(config, tableNameByte);
+
+				HColumnDescriptor[]  hColumnDescriptorList = hTable.getTableDescriptor().getColumnFamilies();
+				List<String> existColumnNamesList = new ArrayList<String>();
+				for (HColumnDescriptor hColumnDescriptor : hColumnDescriptorList)
+				{
+					existColumnNamesList.add(hColumnDescriptor.getNameAsString());
+				}
+
+				// checking if all the column family are in the table, adding it if not.
+
+				List<String> missingColumnFamilies = new ArrayList<String>();
+				for (String coulmnFamily : coulmnFamilyArray)
+				{
+					if (!existColumnNamesList.contains(coulmnFamily))
+					{
+						LOG.warn(coulmnFamily + " does not exist in " + tableName);
+						missingColumnFamilies.add(coulmnFamily);
+					}
+				}
+
+				if (!missingColumnFamilies.isEmpty())
+				{
+					hbase.disableTable(tableNameByte);
+					for (String coulmnFamily : missingColumnFamilies)
+					{
+						hbase.addColumn(tableNameByte, new HColumnDescriptor(coulmnFamily.getBytes()));
+						LOG.info(coulmnFamily + " added to " + tableName);
+					}
+					hbase.enableTable(tableNameByte);
+				}
+
+				return hTable;
+			}
+
+			//table exists but disabled
+			LOG.info("Table: " + tableName + " exists but disabled, deleting the table.");
+			hbase.deleteTable(tableNameByte);
+		}
+
+		//creating a new table
+		HTableDescriptor hTableDescriptor = new HTableDescriptor(tableNameByte);
+		for (String coulmnFamily : coulmnFamilyArray)
+		{
+			HColumnDescriptor meta = new HColumnDescriptor(coulmnFamily.getBytes());
+			hTableDescriptor.addFamily(meta);
+		}
+
+		hbase.createTable(hTableDescriptor);
+		LOG.info("New hBase table created with name: " + tableName);
+
+		return new HTable(config, tableNameByte);
+
+	}
 
 	public boolean makeDirs(JobConf conf) throws IOException {
-		HBaseAdmin hBaseAdmin = getHBaseAdmin();
-
-		if (hBaseAdmin.tableExists(tableName))
-			return true;
-
-		LOG.debug("creating hbase table: {}", tableName);
-
-		HTableDescriptor tableDescriptor = new HTableDescriptor(tableName);
-
-		String[] familyNames = ((HBaseScheme) getScheme()).getFamilyNames();
-
-		for (String familyName : familyNames)
-			tableDescriptor.addFamily(new HColumnDescriptor(familyName));
-
-		hBaseAdmin.createTable(tableDescriptor);
-
+		
+		openTable(tableName, ((HBaseScheme) getScheme()).getFamilyNames());
 		return true;
 	}
 
